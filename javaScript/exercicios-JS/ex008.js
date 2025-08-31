1d75e7430739edf0ef67c30c4d298fedd090bd61
Drizi-b <drizi.b.oliveira@gmail.com>
2025-08-31 01:38:49 -0300

    inclusao

--- javaScript/exercicios-JS/ex008.js
+++
@@ -1,6 +0,0 @@
-let vel = 70
-console.log(`A velocidade do seu carro é de ${vel}Km/h`)
-if (vel > 60) {
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-}
-console.log('Dirija sempre utilizando o cinto de segurança')
